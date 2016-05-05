4284413914b943bf4130260cf1b6151ed136ae39
dospunk <lostboycmd@gmail.com>
2016-05-05 16:22:51 -0400

    fixed screwups

--- 40WattClub.py
+++ 40WattClub.py
@@ -13,7 +13,7 @@ prices = driver.find_elements_by_xpath("//h3[@class='price-range']")
 
 band_max_len = 0
 day_max_len = 0
-door_max_len = 0
+price_max_len = 0
 
 for band in bands:
 	if len(band.text) > band_max_len:
@@ -23,26 +23,26 @@ for day in days:
 	if len(day.text) > day_max_len:
 		day_max_len = len(day.text)
 
-for door in doors:
-	if len(door.text) > door_max_len:
-		door_max_len = len(door.text)
+for price in prices:
+	if len(price.text) > price_max_len:
+		price_max_len = len(price.text)
 
-for i in xrange(0, len(days)):
+for i in range(0, len(days)):
 	bands_text = bands[i].text
 	print('Band: {0}'.format(bands_text), end="")
-	for j in xrange(1, band_max_len - len(bands[i].text)):
+	for j in range(1, band_max_len - len(bands[i].text)):
 		print(" ", end="")
-	print(" ")'
+	print(" ")
 	
 	days_text = days[i].text
 	print('Date: {0}'.format(days_text), end="")
-	for k in xrange(1, day_max_len - len(days[i].text)):
+	for k in range(1, day_max_len - len(days[i].text)):
 		print(" ", end="")
 	print(" ")
 	
 	price_text = prices[i].text
 	print('{0}'.format(price_text), end="")
-	for l in xrange(1, price_max_len - len(prices[i].text)):
+	for l in range(1, price_max_len - len(prices[i].text)):
 		print(" ", end="")
 	print(" ")
 	
