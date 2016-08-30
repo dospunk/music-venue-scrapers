4d67f919146b1db3289206f5efe09b2e1f839942
dospunk <lostboycmd@gmail.com>
2016-08-29 20:02:29 -0400

    Fixed it

--- 40WattClub.py
+++ 40WattClub.py
@@ -2,6 +2,7 @@
 #to save bandwidth
 
 from selenium import webdriver
+import re
 #from selenium.webdriver.firefox.webdriver import FirefoxProfile
 
 #profile = FirefoxProfile("C:\\Users\\user\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\zahyinl2.Selenium")
@@ -15,53 +16,41 @@ debugging = False
 bands = driver.find_elements_by_xpath("//h1[@class='headliners summary']/a[1]")
 days = driver.find_elements_by_xpath("//h2[@class='dates']")
 doors = driver.find_elements_by_xpath("//span[@class='doors']")
-prices = driver.find_elements_by_xpath("//h3[@class='price-range']")
+raw_prices = driver.find_elements_by_class_name("ticket-price")
+prices = [];
+
+u = 0
+
+for p in raw_prices:
+	prices.append(p.text.translate({ord(c): None for c in 'Tickets'}).replace("\n", ""))
+	u = u+1
+
+for pr in range(0, len(prices)):
+	if prices[pr] == "Fr":
+		prices[pr] = "Free"
 
 band_max_len = 0
 day_max_len = 0
 price_max_len = 0
-
-for band in bands:
-	if len(band.text) > band_max_len:
-		band_max_len = len(band.text)
-		
-for day in days:
-	if len(day.text) > day_max_len:
-		day_max_len = len(day.text)
-
-for price in prices:
-	if len(price.text) > price_max_len:
-		price_max_len = len(price.text)
 
 if debugging:
 	print(band_max_len)
 	print(day_max_len)
 	print(price_max_len)
 
-for i in range(0, len(days)):
+for i in reversed(range(0, len(days))):
 	bands_text = bands[i].text
-	print('Band: {0}'.format(bands_text), end="")
-	for j in range(1, band_max_len - len(bands[i].text) + 4):
-		print(" ", end="")
-	print("|", end="")
-	print("    ", end="")
+	print('Band: {0}\n'.format(bands_text), end="")
 	
 	days_text = days[i].text
-	print('Date: {0}'.format(days_text), end="")
-	for k in range(1, day_max_len - len(days[i].text) + 4):
-		print(" ", end="")
-	print("|", end="")
-	print("    ", end="")
-	price_text = prices[i].text
-	print('{0}'.format(price_text), end="")
-	for l in range(1, price_max_len - len(prices[i].text) + 4):
-		print(" ", end="")
-	print("|", end="")
-	print("    ", end="")
+	print('Date: {0}\n'.format(days_text), end="")
+
+	price_text = prices[i]
+	print('Price: {0}\n'.format(price_text), end="")
 	
 	times_text = doors[i].text
-	print('Price: {0}'.format(times_text))
+	print('{0}'.format(times_text))
 	
-	print(" ")
+	print("------------------------------------------------------------")
 
 driver.quit()
