7b26013c04aab8173da52d0a6c6ba9a5feacd885
dospunk <dospunk@users.noreply.github.com>
2016-10-24 15:55:14 -0400

    not using bs4

--- firefox/Cale.py
+++
@@ -1,5 +0,0 @@
-from bs4 import BeautifulSoup as a
-import urllib.request as  c
-d = c.urlopen('https://en.wikipedia.org/wiki/Lorem_ipsum')
-b = a(d.read(), 'html.parser')
-print(b.blockquote.get_text())
