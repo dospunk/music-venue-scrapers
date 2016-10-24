ff5d4bd9a06a89bf0661f312008cfc8f14d884d0
dospunk <lostboycmd@gmail.com>
2016-10-24 16:03:25 -0400

    trying PhantomJS

---
+++ phantom/40WattClub.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+import time
+
+def run():
+	driver = webdriver.PhantomJS()
+	driver.get("http://www.40Watt.com")
+	
+	debugging = False
+	
+	bands = driver.find_elements_by_xpath("//h1[@class='headliners summary']/a[1]")
+	days = driver.find_elements_by_xpath("//h2[@class='dates']")
+	doors = driver.find_elements_by_xpath("//span[@class='doors']")
+	raw_prices = driver.find_elements_by_class_name("ticket-price")
+	prices = [];
+	
+	u = 0
+	
+	for p in raw_prices:
+		prices.append(p.text.translate({ord(c): None for c in 'Tickets'}).replace("\n", ""))
+		u = u+1
+	
+	for pr in range(0, len(prices)):
+		if prices[pr] == "Fr":
+			prices[pr] = "Free"
+	
+	band_max_len = 0
+	day_max_len = 0
+	price_max_len = 0
+	
+	if debugging:
+		print(band_max_len)
+		print(day_max_len)
+		print(price_max_len)
+	
+	for i in reversed(range(0, len(days))):
+		bands_text = bands[i].text
+		print('Band: {0}\n'.format(bands_text), end="")
+		
+		days_text = days[i].text
+		print('Date: {0}\n'.format(days_text), end="")
+	
+		price_text = prices[i]
+		print('Price: {0}\n'.format(price_text), end="")
+		
+		times_text = doors[i].text
+		print('{0}'.format(times_text))
+		
+		print("------------------------------------------------------------")
+	
+	driver.quit()
+	return
+
+while True:
+	run()
+	time.sleep(43200)
