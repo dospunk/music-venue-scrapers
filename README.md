75dff36a9a644187ee4846262a2383d260055a27
dospunk <lostboycmd@gmail.com>
2017-07-25 12:02:01 -0400

    Adds Masquerade scraper

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ A collection of scrapers written in python for music venue websites. All scraper
   - [website](http://www.40watt.com/)
 - [Caledonia Lounge](https://github.com/dospunk/music-venue-scrapers/blob/master/caledonia.py)
   - [website](http://www.caledonialounge.com)
+- [Masquerade](https://github.com/dospunk/music-venue-scrapers/blob/master/masq.py)
+  - [website](http://www.masq.com)
 
 ## Requests
 If you would like a scraper made, open an issue with `[Request]` in the subject.
---
+++ masq.py
@@ -0,0 +1,47 @@
+import requests
+from html.parser import HTMLParser
+
+class MasqParser(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.out = 0
+		self.filter = ["", "Buy Tickets", "More Info", "The Masquerade Presents", "View All Shows"]
+		self.tag_filter = ["script", "title", "style", "meta"]
+		self.attr_filter = [("class", "col col-md-1-4 sidebar")]
+		self.days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+		self.locations = ["Hell", "Purgatory", "Heaven", "Other Location"]
+	
+	def handle_starttag(self, tag, attrs):
+		if tag in self.tag_filter:
+			self.out += 1
+		elif ("id", "featured-carousel slide slick-initialized slick-slider") in attrs:
+			self.out = 100000
+		elif ("class", "col col-md-3-4 upcomingShows js-eventList archive") in attrs:
+			self.out = 0
+		else:
+			for attr in attrs:
+				if attr in self.attr_filter:
+					self.out += 1
+					break
+
+	def handle_endtag(self, tag):
+		if tag in self.tag_filter:
+			self.out -= 1
+
+	def handle_data(self, data):
+		data = data.strip()
+		if data not in self.filter and not self.out:
+			try:
+				int(data)
+				print(data + " ", end="")
+			except ValueError:
+				if data in self.days or data in self.locations:
+					print(data + " ", end="")
+				elif "Doors " in data and "/" in data and ":" in data:
+					print(data)
+					print("------------------------")
+				else:
+					print(data)
+
+parser = MasqParser();
+parser.feed(requests.get("http://masq.com/").text)
